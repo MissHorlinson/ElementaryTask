a6f6731fb809ece83dba8aa4a5186b71c825b033
Maria <mary24018@gmail.com>
2019-10-11 08:43:14 +0300

    Inital commit

--- src/main/java/help/InstructionAndParse.java
+++ src/main/java/help/InstructionAndParse.java
@@ -6,9 +6,7 @@ public class InstructionAndParse {
 
     static Scanner in = new Scanner(System.in);
 
-    public static void chessBoardDescription(){
-        System.out.println("Task 1: print the chess board to screen\n---------------------------------------");
-    }
+
 
     public static int chessBoardInstructionAndParse(String side){
         int sideValue = 0;
